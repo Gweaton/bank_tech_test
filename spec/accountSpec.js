7e8976ef09f4dcfeec7158ed05d0ef4e5bd9ed0f
George Eaton <george_eaton@hotmail.co.uk>
2017-01-23 10:24:08 +0000

    account has starting balance of 0

---
+++ spec/accountSpec.js
@@ -0,0 +1,13 @@
+describe("Account", function() {
+  var account;
+});
+
+  beforeEach(function() {
+    account = new Account();
+  });
+
+  describe("balance", function() {
+    it("should start on 0 for a new account", function() {
+      expect(account.balance).toEqual(0);
+    });
+  });
